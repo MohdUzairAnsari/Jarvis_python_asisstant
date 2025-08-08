d15db0ce976d382cd95f56caf305056c33801f4d
MohdUzairAnsari <150615636+MohdUzairAnsari@users.noreply.github.com>
2025-08-09 00:24:27 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "skyfall" : "https://youtu.be/DeumyOzKqgI?si=K8iDKm2ZGxtgT1yY",
+    "diet" : "https://youtu.be/1nPi_r5z_Mk?si=NFpDv9gLeHHo19H9",
+    "die" : "https://youtu.be/zgaCZOQCpp8?si=Y7zxaxvwjF5mezfT",
+    "criminal" : "https://youtu.be/SaTkorLH1lo?si=gnlpWTfrUY1zjIfg",
+    "not" : "https://youtu.be/T6eK-2OQtew?si=CgggBb5zW0hvMDcy"
+
+}
